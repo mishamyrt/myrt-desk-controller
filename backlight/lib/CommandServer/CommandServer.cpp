c7779a9a4ccdaae3ae38a757910f95463cb4947c
mishamyrt <misha@myrt.co>
2022-04-28 00:29:30 +0400

    Avoid inline functions

---
+++ backlight/lib/CommandServer/CommandServer.cpp
@@ -0,0 +1,26 @@
+#include "CommandServer.h"
+
+void CommandServer::begin(Stream *serial) {
+  _data.setHandler(this);
+  _data.connect(serial);
+}
+
+CommandServer& CommandServer::on(uint8_t code, bool (*handler)(uint8_t *message, uint8_t length)) {
+  _handlers[_handlers_count].code = code;
+  _handlers[_handlers_count].handle = handler;
+  _handlers_count++;
+  return *this;
+}
+
+void CommandServer::handle() {
+  _data.handle();
+}
+
+bool CommandServer::handleMessage(uint8_t *message, uint8_t length) {
+  for (uint8_t i = 0; i < _handlers_count; i++) {
+    if (_handlers[i].code == message[0]) {
+      return _handlers[i].handle(message, length);
+    }
+  }
+  return false;
+}
--- backlight/lib/CommandServer/CommandServer.h
+++ backlight/lib/CommandServer/CommandServer.h
@@ -12,30 +12,10 @@ struct CommandHandler {
 // Command wrapper around Dap slave
 class CommandServer : public DapHandler {
   public:
-    void begin(Stream *serial) {
-      _data.setHandler(this);
-      _data.connect(serial);
-    }
-
-    CommandServer& on(uint8_t code, bool (*handler)(uint8_t *message, uint8_t length)) {
-      _handlers[_handlers_count].code = code;
-      _handlers[_handlers_count].handle = handler;
-      _handlers_count++;
-      return *this;
-    }
-
-    void handle() {
-      _data.handle();
-    }
-
-    bool handleMessage(uint8_t *message, uint8_t length) {
-      for (uint8_t i = 0; i < _handlers_count; i++) {
-        if (_handlers[i].code == message[0]) {
-          return _handlers[i].handle(message, length);
-        }
-      }
-      return false;
-    }
+    void begin(Stream *serial);
+    void handle();
+    bool handleMessage(uint8_t *message, uint8_t length);
+    CommandServer& on(uint8_t code, bool (*handler)(uint8_t *message, uint8_t length));
 
   private:
     CommandHandler _handlers[32];
